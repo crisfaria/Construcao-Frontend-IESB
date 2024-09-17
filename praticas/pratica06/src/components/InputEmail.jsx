50f2856375844490ea966174a44c36520d230c20
Cristina Faria <crisfaria@leandrofaria.com>
2024-09-16 21:05:03 -0300

    Prática 06

---
+++ praticas/pratica06/src/components/InputEmail.jsx
@@ -0,0 +1,17 @@
+function InputEmail(props) {
+  return (
+    <>
+      <label htmlFor="email">E-mail</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={props.email}
+        onChange={props.mudaValor}
+        required
+      />
+    </>
+  );
+}
+
+export default InputEmail;
